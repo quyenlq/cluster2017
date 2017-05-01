a8e83418716bd60739b6e1553e0bd0e7481084ab
Quốc Quyền Lục <lucquocquyen@gmail.com>
2017-05-02 01:40:54 +0300

    finish draft

---
+++ qq_kmeans.py
@@ -0,0 +1,142 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pdb
+from solution import Solution, Point
+
+
+# M: number of clusters
+# return a new solution with M random centroids
+def gen_initial_solution(data, M):
+	random_centroids = data[np.random.choice(np.arange(len(data)),M)]
+	centroid_points = np.array([])
+	i = 0
+	for c in random_centroids:
+		centroid_point = Point(c, label=i)
+		i+=1
+		centroid_points = np.append(centroid_points,centroid_point)
+	# Point class wrapper
+	data_points = np.array([Point(x) for x in data])
+	return Solution(centroid_points,data_points)
+
+# f = data, c = number of cluster
+def ga(data,c):
+	solutions = np.array([])
+	for s in range(S):
+		print 'Gen inital solution %d' %s
+		new_solution = gen_initial_solution(data,c)
+		new_solution.partition()
+		print 'Partition new solution %d complete' % s
+		solutions=np.append(solutions,new_solution)
+		print 'Added solution %d to solution set' % s
+	top_solutions = np.array([])
+	for t in range(T):
+		print 'Start gen new solutions from solution set'
+		new_solutions=gen_new_solutions(solutions)
+		print 'Gen completed, pick best'
+		best_solution = get_best_solutions(new_solutions)
+		print 'Picked best, added to top'
+		top_solutions=np.append(top_solutions,best_solution)
+	print 'Finish, return top '
+	return get_best_solutions(top_solutions)
+
+def get_best_solutions(sols):
+	print 'Chose best from sols'
+	print [s.MSE() for s in sols]
+	return sols[np.argmin([s.MSE() for s in sols])]
+
+# Gen new solutions from initial solutions
+# repeat S times to generate S solutions
+def gen_new_solutions(solutions):
+	new_solutions = np.array([])
+	for i in range(S):
+		print 'Pick pair of solutions %d' %i
+		solution_pair = get_pair(solutions)
+		new_solution = cross_over(solution_pair)
+		new_solutions=np.append(new_solutions,new_solution)
+	return new_solutions
+
+# Crossover 2 solution to generate one solution
+def cross_over(pair):
+	print 'Start crossover, choose from sol pair'
+	sol1 = pair[0]
+	sol2 = pair[1]
+	combined_centroids = combine_centroids(sol1.centroids_,sol2.centroids_)
+	combined_partitions = combine_partitions(sol1,sol2,combined_centroids)
+	print 'Combined centroids size: %d, combined parition size: %d' %(combined_centroids.size, combined_partitions.size)
+	sol = Solution(combined_centroids,combined_partitions)
+	sol.update_centroid()
+	sol.remove_empty_clusters()
+	print 'Remove empty cluster, now sol has %d clusters' %(sol.centroids_.size)
+	sol.relabel()
+	print 'Relabel cluster'
+	sol.display()
+	return pnn(sol)
+
+
+# TODO: Add K as paremetter
+def pnn(solution, K = 15):
+	solution.find_nearest_neighbor()
+	while solution.centroids_.size >= K:
+		solution.mergePNN()
+	solution.relabel()
+	return solution
+
+
+def get_pair(solutions):
+	pair = np.random.choice(solutions, 2)
+	while pair[0]==pair[1]:
+		pair = np.random.choice(solutions, 2)
+	return pair
+
+def combine_centroids(c1,c2):
+	k = np.max([c.label_ for c in c1]) +1
+	c_new = np.copy(c2)
+	for c in c_new:
+		c.label_+=k
+	c_com = np.append(c1,c_new)
+	return c_com
+
+def combine_partitions(sol1,sol2,c_com):
+	k = c_com.size/2
+	N = sol1.points_.size
+	combine_p = np.array([])
+	for i in range(N):
+		# x1,x2 are the same points
+		# x: new point
+		x = sol1.points_[i]
+		d1 = sol1.distance_to_centroid(x,c_com[x.label_])
+		d2 = sol2.distance_to_centroid(x,c_com[x.label_+k])
+		if d1 < d2:
+			x = Point(x.xy_, x.label_)
+		else:
+			x = Point(x.xy_, x.label_+ k)
+		combine_p=np.append(combine_p,x)
+	return combine_p
+
+
+
+################################################################################################
+# MAIN
+################################################################################################
+
+
+k = [15,15,15,15,20,35,50,8,100,100,16];
+files = ['s1','s2','s3','s4', 'a1', 'a2', 'a3','unbalance','birch1', 'birch2','dim32']
+S = 5
+T = 3
+def main():
+	np.random.seed(1)
+	for t in range(T):
+		for f,c in zip(files,k):
+			print 'Running data set %s' %f
+			fi = open(f+'.txt')
+			fi_gt = open(f+'-gt.txt')
+			data = np.loadtxt(fi)
+			gt = np.loadtxt(fi_gt)
+			# ga(data,c)
+			sol=ga(data,c)
+			print sol.MSE()
+			break
+
+main()
+
--- solution.py
+++ solution.py
@@ -1,18 +1,156 @@
 import numpy as np
-
+import pdb
 class Solution:
+	# nn_: Point list of nearest neighbor
+	# dists_: Float list of distances to nearest neighbor
+	nn_ = None
+	dists_ = None
+
 	def __init__(self, c,p):
 		self.centroids_ = c
-		self.points = p
+		self.points_ = p
 
+	# Partition
+	def partition(self):
+		for p in self.points_:
+			dists = np.array([])
+			for c in self.centroids_:
+				dist = np.dot(p.xy_-c.xy_,p.xy_-c.xy_)
+				dists = np.append(dists, dist)
+			centroid = self.centroids_[np.argmin(dists)]
+			p.label_ = centroid.label_
+		self.update_centroid()
+
+	# Update centroids
+	def update_centroid(self):
+		for c in self.centroids_:
+			lbl = c.label_
+			points = self.get_partition(lbl)
+			new_c_xy = np.mean([p.xy_ for p in points])
+			c.xy_ = new_c_xy
+
+	# Calculate mean square error
 	def MSE(self):
-		return 0
+		TSE = 0
+		for c in self.centroids_:
+			p = self.get_partition(c.label_)
+			for x in p:
+				e = np.dot(x.xy_-c.xy_,x.xy_-c.xy_)
+				TSE+=e
 
-	def get_partition(label):
-		return [for x in points if x.label_==label]
+		return TSE/self.points_.size
 
-	def get_centroid(label):
-		return [for c in centroids if c.label_==label]
+
+	# Distance from a point to its centroid
+	@staticmethod
+	def distance_to_centroid(x, c):
+		# c = self.get_centroid(x.label_)
+		return np.dot(x.xy_-c.xy_,x.xy_-c.xy_)
+
+	# Get partition from label
+	def get_partition(self,label):
+		return np.array([x for x in self.points_ if x.label_==label])
+
+	# Get centroid point from label
+	def get_centroid(self,label):
+		return np.array([c for c in self.centroids_ if c.label_==label])
+
+	# Remove cluster with size == 0
+	def remove_empty_clusters(self):
+		# Get cluster centroid with partition size = 0
+		empty_c = np.array([c for c in self.centroids_ if self.get_partition(c.label_).size==0])
+		# Get index of these partitions
+		empty_c_idx = np.array([c.label_ for c in empty_c])
+		# If exists nearest neighbor array, remove that of deleted cluster
+		if self.nn_ is not None:
+			# empty_nn = self.nn_[empty_c_idx]
+			self.nn_ = np.delete(self.nn_,empty_c_idx)
+		# Similar to dists array
+		if self.dists_ is not None:
+			# empty_dists = self.dists_[empty_c_idx]
+			self.dists_ = np.delete(self.dists_, empty_c_idx)
+		self.centroids_ = np.delete(self.centroids_, empty_c_idx)
+
+	# Relabel from zero to size of centroids
+	def relabel(self):
+		i = 0
+		for c in self.centroids_:
+			print 'Rename label %d to label %d'
+			p = self.get_partition(c.label_)
+			for x in p:
+				x.label_=i
+			c.label_=i
+			i+=1
+
+	# Find nearest neighbor, save list of nearest neighbor and corresponding
+	# distance to object attributes nn_ & dists_
+	def find_nearest_neighbor(self):
+		self.nn_ = np.array([])
+		self.dists_ = np.array([])
+		for a in self.centroids_:
+			na = self.get_partition(a.label_).size
+			nn_c = None
+			min_dist = -1
+			for b in self.centroids_:
+				if b!=a:
+					nb = self.get_partition(b.label_).size
+					dist = (np.dot(a.xy_-b.xy_,a.xy_-b.xy_)*na*nb)/(na+nb)
+					if dist<min_dist or min_dist == -1:
+						nn_c = b
+						min_dist=dist
+			self.nn_ = np.append(self.nn_,nn_c)
+			self.dists_ = np.append(self.dists_, min_dist)
+
+	# Update nearest neighbor after merge two cluster
+	def updateNN(self, lbl1, lbl2):
+		for idx,a in enumerate(self.centroids_):
+			# For each centroid of solution
+			# find a new neighbor only if this has been
+			# changed because of merging
+			nearest_c = self.nn_[idx]
+			if nearest_c.label_==lbl1 or nearest_c.label_==lbl2:
+				na = self.get_partition(a.label_).size
+				nn_c = None
+				min_dist = -1
+				for b in self.centroids_:
+					if b != a and b!=nearest_c:
+						nb = self.get_partition(b.label_).size
+						dist = (np.dot(a.xy_ - b.xy_, a.xy_ - b.xy_) * na * nb) / (na + nb)
+						if dist < min_dist or min_dist == -1:
+							nn_c = b
+							min_dist = dist
+				self.nn_[idx] = nn_c
+				self.dists_[idx] = min_dist
+
+	# Merge two clusters which have smallest merging cost function
+	def mergePNN(self):
+		# Get current nearest pair
+		# lbl1: index of smallest value in dists
+		# equivalent to first label of nearest pair
+		# for example: dists = [ 3 2 (1) 4 ], nn_ = [c2,c4,(c3),c1]
+		# then lbl1 = 2, c2 = nn_[lbl1]= c3 -> lbl2 = c3.label_
+		lbl1 = np.argmin(self.dists_)
+		c1 = self.centroids_[lbl1]
+		c2 = self.nn_[lbl1]
+		lbl2 = c2.label_
+
+		p1 = self.get_partition(lbl1)
+		n1 = p1.size
+		p2 = self.get_partition(lbl2)
+		n2 = p2.size
+		# Move centroid c1
+		cmerge_xy = ((n1*c1.xy_) + (n2*c2.xy_))/(n1+n2)
+		c1.xy_ = cmerge_xy
+		# Change label of partition
+		# no need to change points in p1
+		for p in p2:
+			p.label_ = c1.label_
+		# Update nearest neighbors
+		self.updateNN(lbl1,lbl2)
+		self.remove_empty_clusters()
+
+	def display(self):
+		print([p.label_ for p in self.points_])
 
 class Point:
 	def __init__(self,xy,label=-1):
@@ -24,3 +162,6 @@ class Point:
 
 	def getY(self):
 		return self.xy[1]
+
+	def __str__(self):
+		return  "%d: %.2f %.2f" %(self.label_,self.xy_[0], self.xy_[1])
